67bfb0092a02369490d7581097c98c3917bd1454
Isam <isamov2012@hotmail.com>
2023-03-18 10:22:43 +0100

    Add project files.

---
+++ Dogs2Mvc/Controllers/DogsController.cs
@@ -0,0 +1,54 @@
+﻿using Dogs2Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dogs2Mvc.Controllers
+{
+	public class DogsController : Controller
+	{
+		static DataService dataService=new DataService();
+		[HttpGet("")]
+		public IActionResult Index()
+		{
+			Dog[] model = dataService.GetAll();
+			return View(model);
+		}
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost("Create")]
+        public IActionResult Create(Dog dog)
+        {
+            if(!ModelState.IsValid) 
+            return View();
+            dataService.AddDog(dog);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost("Update/{id}")]
+        public IActionResult Update(int id, Dog dog)
+        {
+            dataService.Update(id, dog);
+            return RedirectToAction(nameof(Index));
+        }
+		[HttpGet("Update/{id}")]
+		public IActionResult Update(int id)
+		{
+			var model = dataService.FindById(id);
+			return View(model);
+		}
+		[HttpGet("Delete")]
+		public IActionResult Delete(int id)
+		{
+			var model = dataService.FindById(id);
+			return View(model);
+		}
+		[HttpPost("Update/{id}")]
+		public IActionResult Delete(int id,Dog dog)
+		{
+			var model = dataService.FindById(id);
+			return View(model);
+		}
+	}
+}
---
+++ Dogs2Mvc/Models/DataService.cs
@@ -0,0 +1,36 @@
+﻿namespace Dogs2Mvc.Models
+{
+	public class DataService
+	{
+		List<Dog> dogs = new List<Dog>
+		{
+			new Dog{Id=1, Name="Char",Age=2},
+			new Dog{Id=2, Name="Walter",Age=1},
+			new Dog{Id=3, Name="Petya",Age=3},
+			new Dog{Id=4, Name="SnowF",Age=5},
+		};
+		public void AddDog(Dog dog)
+		{
+			dog.Id=dogs.Max(x => x.Id)+1;
+			dogs.Add(dog);
+		}
+
+		public Dog[] GetAll()
+		{
+			return dogs.ToArray();
+		}
+
+		public Dog FindById(int id)
+		{
+			return dogs
+				.SingleOrDefault(d => d.Id==id);
+		}
+
+		public void Update(int id,Dog newDog)
+		{
+			var dog = FindById(id);
+			dog.Name = newDog.Name;
+			dog.Age = newDog.Age;
+		}
+	}
+}
